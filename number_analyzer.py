73e76c20b431020a819ec90f406d8d2b85d324cc
adisu3358-sys <adisu3358@gmail.com>
2026-02-26 09:56:21 +0300

    Update number_analyzer.py
    step of python programming

--- number_analyzer.py
+++ number_analyzer.py
@@ -1,5 +1,12 @@
+# step1: data collection 
 numbers = []
-for I in range(5);
+for i in range(5);
 num= int(input("Enter numbers:"))
 numbers.append(num)
 print ("your numbers are:"numbers )
+#step 2: summary statics
+Total=sum(numbers)
+average=Total/len(numbers)
+largest= max(numbers)
+smallest= min(numbers)
+step 3:count odd and even number 
